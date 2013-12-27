ce201c29c7654a44e1c671e26578b8f9b35caf76
Alexandre Carlos <alexandre.carlos@gmail.com>
2013-12-27 23:28:20 +0000

    Escrita de uma avaliação simples

--- pt/bportugal/Avaliacoes/Consolidation.py
+++ pt/bportugal/Avaliacoes/Consolidation.py
@@ -7,6 +7,9 @@
 __author__ = 'Alexandre Carlos'
 __version__ = "0.1"
 
+from xlrd import open_workbook
+from xlwt import Workbook
+
 class Worker(object):
     """
     """
@@ -25,6 +28,23 @@ class Worker(object):
     def get_assessment(self):
         return self.evaluation
 
+class WorkerEvaluation(object):
+    """
+
+    """
+
+    def __init__(self, exel_file):
+        self.worker_file = exel_file
+
+    def getEvaluation(self):
+
+        wb = open_workbook(self.worker_file)
+
+        sheet = wb.sheet_by_name(u"Avaliação Global")
+
+        # sheet.cell(row_index,col_index).value
+        return sheet.cell(2,3).value
+
 
 class Consolidation(object):
     """
@@ -32,26 +52,39 @@ class Consolidation(object):
     """
 
     def __init__(self):
-        self.worker_excel_file = r"C:\Users\KKU035\Documents\Dropbox\Gestão\Pessoal\Avaliações 2013\Avaliação\António Simões.xls"
+        # self.worker_excel_file = r"C:\Users\KKU035\Documents\Dropbox\Gestão\Pessoal\Avaliações 2013\Avaliação\António Simões.xls"
+        self.worker_excel_file = r"/home/alexandre/Documentos/Dropbox/Gestão/Pessoal/Avaliações 2013/Avaliação/António Simões.xls"
         self.worker = "António Simões"
-        self.global_assessment_excel_file = r"C:\Users\KKU035\Documents\Dropbox\Gestão\Pessoal\Avaliações 2013\Avaliação\AvaliaçõesGlobais.xls"
+        # self.global_assessment_excel_file = r"C:\Users\KKU035\Documents\Dropbox\Gestão\Pessoal\Avaliações 2013\Avaliação\AvaliaçõesGlobais.xls"
+        self.global_assessment_excel_file = r"/home/alexandre/Documentos/Dropbox/Gestão/Pessoal/Avaliações 2013/Avaliação/AvaliaçõesGlobais.xls"
         self.workers = ()
+        self.global_wb = Workbook(encoding='utf-8')
 
     def get_workers(self):
-        self.workers = ("António Simões")
-
+        self.workers = ("António Simões",)
 
     def handle_worker(self, nome):
         worker = Worker(nome)
 
-        worker.put_assessment(3)
+        wk = WorkerEvaluation(self.worker_excel_file)
+
+        worker.put_assessment(wk.getEvaluation())
 
         return worker
 
+    def save_assessment(self, worker):
 
-    def save_assessment(selfself, worker):
+        worker_name = worker.get_name()
 
-        return -88
+        print(worker_name)
+
+        sheet2 = self.global_wb.add_sheet(u"Avaliação Global")
+        sheet2.write(0,0,worker_name)
+        sheet2.write(1,0,worker.get_assessment())
+
+        self.global_wb.save(self.global_assessment_excel_file)
+
+        return 10
 
     def do_consolidation(self):
         """
--- pt/bportugal/Avaliacoes/testes/ConsolidationTest.py
+++ pt/bportugal/Avaliacoes/testes/ConsolidationTest.py
@@ -4,20 +4,32 @@
 __author__ = 'Alexandre Carlos'
 
 import unittest
-from pt.bportugal.Avaliacoes.Consolidation import Consolidation
+from pt.bportugal.Avaliacoes.Consolidation import *
 
 class ConsolidationTestCase(unittest.TestCase):
 
     def setUp(self):
         self.consolidation = Consolidation()
+        self.we = WorkerEvaluation(self.consolidation.worker_excel_file)
 
     def test_init(self):
-        self.assertEqual(self.consolidation.worker_excel_file, r"C:\Users\KKU035\Documents\Dropbox\Gestão\Pessoal\Avaliações 2013\Avaliação\António Simões.xls")
+        # self.assertEqual(self.consolidation.worker_excel_file, r"C:\Users\KKU035\Documents\Dropbox\Gestão\Pessoal\Avaliações 2013\Avaliação\António Simões.xls")
+        self.assertEqual(self.consolidation.worker_excel_file, r"/home/alexandre/Documentos/Dropbox/Gestão/Pessoal/Avaliações 2013/Avaliação/António Simões.xls")
         self.assertEqual(self.consolidation.worker, "António Simões")
-        self.assertEqual(self.consolidation.global_assessment_excel_file, r"C:\Users\KKU035\Documents\Dropbox\Gestão\Pessoal\Avaliações 2013\Avaliação\AvaliaçõesGlobais.xls")
+        # self.assertEqual(self.consolidation.global_assessment_excel_file, r"C:\Users\KKU035\Documents\Dropbox\Gestão\Pessoal\Avaliações 2013\Avaliação\AvaliaçõesGlobais.xls")
+        self.assertEqual(self.consolidation.global_assessment_excel_file, r"/home/alexandre/Documentos/Dropbox/Gestão/Pessoal/Avaliações 2013/Avaliação/AvaliaçõesGlobais.xls")
         self.assertEqual(self.consolidation.workers, ())
         # self.assertEqual(True, False)
 
+    def test_worker_evaluation (self):
+        evaluation = self.we.getEvaluation()
+        self.assertEqual(5.500, evaluation)
+
+    def test_handle_worker(self):
+        wk = self.consolidation.handle_worker(self.consolidation.worker)
+
+        self.assertEqual("António Simões", wk.get_name())
+        self.assertEqual(5.500, wk.get_assessment())
 
 if __name__ == '__main__':
     unittest.main()
